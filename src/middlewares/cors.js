a8531cb402add61197201da9492652e88bda1f00
Lacciman11 <nwabekeyiprecious@gmail.com>
2025-04-23 21:57:56 +0100

    fourth commit

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 
 const corsOptions = {
   origin: ['http://localhost:5173', 'http://127.0.0.1'],
-  methods: ['POST', 'GET', 'PATCH', 'OPTIONS'],
+  methods: ['POST', 'GET', 'PUT','DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
